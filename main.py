0f92d444e5de025d6a39d5d9c171b228063de66c
geekiskatel <65942953+geekiskatel@users.noreply.github.com>
2023-12-03 18:31:52 +0300

    Update main.py
    Добавление документации

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ pygame.init()
 
 # Ширина и Длина Окна с игрой
 WIDTH, HEIGHT = 800, 600
-# Кол-во кадров в секунду
+# Кол-во кадров в секунду (так как расчет времени идет от этого значения,
+# то при увеличении FPS будет увеличена скорость игры)
 FPS = 60
-# Ширина и Высота изображения с текстурой (хитбокс)
+# Размер области, которая выделяется под каждый объект
 TILE = 32
 
 # Создание окна
@@ -24,22 +25,9 @@ imgBrick = pygame.image.load('images/block_brick.png')
 # Загрузка текстуры логотипа ВШЭ
 imgHSE = pygame.image.load('images/HSE_Pixel.jpg')
 # Загрузка текстуры танков на разных рангах
-imgTanks = [
-    pygame.image.load('images/tank1.png'),
-    pygame.image.load('images/tank2.png'),
-    pygame.image.load('images/tank3.png'),
-    pygame.image.load('images/tank4.png'),
-    pygame.image.load('images/tank5.png'),
-    pygame.image.load('images/tank6.png'),
-    pygame.image.load('images/tank7.png'),
-    pygame.image.load('images/tank8.png'),
-]
+imgTanks = [pygame.image.load(f'images/tank{i}.png') for i in range(1, 9)]
 # Загрузка текстуры взрыва, для создания анимации
-imgBangs = [
-    pygame.image.load('images/bang1.png'),
-    pygame.image.load('images/bang2.png'),
-    pygame.image.load('images/bang3.png'),
-]
+imgBangs = [pygame.image.load(f'images/bang{i}.png') for i in range(1, 4)]
 # Загрузка текстуры бонусов
 imgBonuses = [
     pygame.image.load('images/bonus_star.png'),
@@ -71,13 +59,22 @@ BULLET_DAMAGE = [1, 1, 2, 3, 2, 2, 3, 4]
 # Значения скорости выстрела в зависимости от ранга
 SHOT_DELAY = [60, 50, 30, 40, 30, 25, 25, 30]
 
-# Класс Интерфейса
+
 class UI:
+    """Class for displaying the user interface in the game.
+
+    Methods:
+        update(): Empty method, needed for compatibility.
+        draw(): Draws UI elements on the game window.
+    """
+
     # Без update() вылазит ошибка
     def update(self):
+        """Updates the UI elements. (Currently does nothing)"""
         pass
 
     def draw(self):
+        """Draws UI elements on the game window."""
         # Подсчитываем игроков
         i = 0
         # Перебираем все объекты
@@ -90,21 +87,46 @@ class UI:
                 text = fontUI.render(str(obj.rank), 1, 'black')
                 # Формирование позиции текста по центру
                 rect = text.get_rect(center=(5 + i * 70 + 11, 5 + 11))
-                # Вывод самого текста
+                # Вывод самого текста (ранг)
                 window.blit(text, rect)
                 # Формирование текстового поля с данными об игроке (здоровье)
                 text = fontUI.render(str(obj.hp), 1, obj.color)
                 # Формирование позиции текста по центру
                 rect = text.get_rect(center=(5 + i * 70 + 32, 5 + 11))
-                # Вывод самого текста
+                # Вывод самого текста (хп)
                 window.blit(text, rect)
                 # Чтобы каждый данные игрока отображались по координатам, иначе пропадет
                 i += 1
 
-# Создание класса Танка
+
 class Tank:
-    # Создание таких параметров как цвет, координата по х и у, направление куда смотрит танк, кнопки управления
+    """Class representing a tank in the game.
+
+    Attributes:
+        color (str): Color of the tank.
+        px (int): Horizontal position of the tank.
+        py (int): Vertical position of the tank.
+        direct (list): Direction the tank is facing.
+        keyList (list): List of control keys for the tank.
+
+    Methods:
+        __init__(self, color, px, py, direct, keyList): Initializes a new tank instance.
+        update(self): Updates the tank's state, including movement based on player input,
+        direction facing, and shooting mechanics.
+        draw(self): Draws the tank on the game window.
+        damage(self, value): Applies damage to the tank and handles its destruction if health falls below zero.
+    """
+
     def __init__(self, color, px, py, direct, keyList):
+        """Initializes a tank with given parameters.
+
+        Args:
+            color (str): Color of the tank.
+            px (int): Initial horizontal position of the tank.
+            py (int): Initial vertical position of the tank.
+            direct (list of int): Initial direction of the tank.
+            keyList (list of int): Control keys for the tank.
+        """
         # Занесение себя в список объектов
         objects.append(self)
         # Указание типа объекта, чтобы программа различала объекты
@@ -121,15 +143,6 @@ class Tank:
         # Задержка между выстрелами
         self.shotTimer = 0
 
-        # Скорость танка
-        self.moveSpeed = 50
-        # Задержка
-        self.shotDelay = FPS
-        # Скорость пули
-        self.bulletSpeed = 5
-        # Урон от пули
-        self.bulletDamage = 1
-
         # Указание кнопок с управлением
         self.keyLEFT = keyList[0]
         self.keyRIGHT = keyList[1]
@@ -140,12 +153,9 @@ class Tank:
 
         # Начальный ранг танка
         self.rank = 0
-        # Разворот картинки в зависимости от
-        self.image = pygame.transform.rotate(imgTanks[self.rank], -self.direct * 90)
-        self.rect = self.image.get_rect(center=self.rect.center)
 
-    # Обновление значений и состояний танка
     def update(self):
+        """Updates the state of the tank, including its position, direction, and shooting parameters."""
         # Обновление модели танка в зависимости от ранга
         self.image = pygame.transform.rotate(imgTanks[self.rank], -self.direct * 90)
         # Обновление параметров танка по таблице рангов:
@@ -200,15 +210,21 @@ class Tank:
             # Создание задержки
             self.shotTimer = self.shotDelay
         # Постепенное снятие задержки
-        if self.shotTimer > 0: self.shotTimer -= 1
+        if self.shotTimer > 0:
+            self.shotTimer -= 1
 
-    # Функция отрисовки танка
     def draw(self):
-        # Отрисовка Танка
+        """Draws the tank on the game field."""
         window.blit(self.image, self.rect)
 
-    # Функция получения урона и гибели танка
     def damage(self, value):
+        """Applies damage to the tank and handles its destruction if necessary.
+
+        Args:
+            value (int): Amount of damage to be applied to the tank.
+        Returns:
+            None
+        """
         # Отнимание здоровья
         self.hp -= value
         # Звук получения урона
@@ -220,30 +236,50 @@ class Tank:
             # Звук смерти танка
             sound_finish.play()
 
-# Класс пули
+
 class Bullet:
-    # Создание таких параметров как танк, который выпускает, координаты полета (вектор), урон от неё
+    """Class representing a bullet in the game.
+
+    Attributes:
+        parent (Tank): The tank that fired the bullet.
+        px (float): Horizontal position of the bullet.
+        py (float): Vertical position of the bullet.
+        dx (float): Horizontal velocity of the bullet.
+        dy (float): Vertical velocity of the bullet.
+        damage (int): Damage inflicted by the bullet.
+
+    Methods:
+        __init__(self, parent, px, py, dx, dy, damage): Initializes a new bullet instance.
+        update(self): Updates the bullet's position and checks for collisions with other game objects.
+        draw(self): Draws the bullet on the game window.
+    """
+
     def __init__(self, parent, px, py, dx, dy, damage):
+        """Initializes a new bullet with given parameters.
+
+        Args:
+            parent (Tank): The tank that fired the bullet.
+            px (float): Initial horizontal position of the bullet.
+            py (float): Initial vertical position of the bullet.
+            dx (float): Horizontal velocity of the bullet.
+            dy (float): Vertical velocity of the bullet.
+            damage (int): Damage inflicted by the bullet.
+        """
         # Добавление пули в объекты
         bullets.append(self)
         # Звук выстрела
         sound_shot.play()
-        # Указание танка владельца
         self.parent = parent
-        # Указание координат
         self.px, self.py = px, py
-        # Указание вектора полета
         self.dx, self.dy = dx, dy
-        # Указание её урона
         self.damage = damage
 
-    # Обновление состояний пуль
     def update(self):
+        """Updates the bullet's position and handles its collision and removal."""
         # Вектор по Х
         self.px += self.dx
         # Вектор по Y
         self.py += self.dy
-
         # Если пуля выходит за пределы окна, то удалить её
         if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
             bullets.remove(self)
@@ -265,31 +301,49 @@ class Bullet:
                         break
 
     def draw(self):
+        """Draws the bullet on the game window."""
         pygame.draw.circle(window, 'yellow', (self.px, self.py), 2)
 
-# Класс взрыва
+
 class Bang:
-    # Создание параметров координат
+    """Class representing an explosion in the game.
+
+    Attributes:
+        px (int): Horizontal position of the explosion.
+        py (int): Vertical position of the explosion.
+
+    Methods:
+        __init__(self, px, py): Initializes a new explosion instance.
+        update(self): Updates the explosion animation frame and removes the explosion after it ends.
+        draw(self): Draws the explosion animation on the game window.
+    """
+
     def __init__(self, px, py):
+        """Initializes a new explosion with given parameters.
+
+        Args:
+            px (int): Horizontal position of the explosion.
+            py (int): Vertical position of the explosion.
+        """
         # Добавление себя в объекты
         objects.append(self)
         # Тип в объектах
         self.type = 'bang'
-
         # Указание координат взрыва
         self.px, self.py = px, py
         # Переменная для увеличения картинки
         self.frame = 0
 
-    # Обновление состояний взрыва
     def update(self):
         # Увеличение фрейма до 3
+        """Updates the state of the explosion, including its animation frame."""
         self.frame += 0.2
         # При 3 и более удаление объекта
-        if self.frame >= 3: objects.remove(self)
+        if self.frame >= 3:
+            objects.remove(self)
 
-    # Отрисовка
     def draw(self):
+        """Draws the explosion on the game window."""
         # Переменная с картинкой взрыва
         image = imgBangs[int(self.frame)]
         # Генерация координат
@@ -297,10 +351,30 @@ class Bang:
         # Отрисовка картинки по координатам
         window.blit(image, rect)
 
-# Класс блока
+
 class Block:
-    # Указание координат и размера
+    """Class representing a block in the game.
+
+    Attributes:
+        px (int): Horizontal position of the block.
+        py (int): Vertical position of the block.
+        size (int): Size of the block.
+
+    Methods:
+        __init__(self, px, py, size): Initializes a new block instance.
+        update(self): Currently does nothing (placeholder for future functionality).
+        draw(self): Draws the block on the game window.
+        damage(self, value): Applies damage to the block, potentially destroying it.
+    """
+
     def __init__(self, px, py, size):
+        """Initializes a new block with given parameters.
+
+        Args:
+            px (int): Horizontal position of the block.
+            py (int): Vertical position of the block.
+            size (int): Size of the block.
+        """
         # Добавление в объекты
         objects.append(self)
         # Добавление типа
@@ -312,39 +386,68 @@ class Block:
 
     # Объект не обновляется и статичен (кроме уничтожения), но отсутствие update() вызовет ошибку
     def update(self):
+        """Updates the block's state. (Currently does nothing)"""
         pass
 
     # Отрисовка блока
     def draw(self):
+        """Draws the block on the game window."""
         window.blit(imgBrick, self.rect)
 
-    # Уничтожение блока
     def damage(self, value):
+        """Applies damage to the block and handles its destruction if necessary.
+
+        Args:
+            value (int): Amount of damage to be applied to the block.
+        Returns:
+            None
+        """
         # Отнятие хп у блока при попадании
         self.hp -= value
         # Если уничтожить - объект пропадет
-        if self.hp <= 0: objects.remove(self)
+        if self.hp <= 0:
+            objects.remove(self)
         # Звук уничтожения
         sound_destroy.play()
 
-# Класс бонуса
+
 class Bonus:
-    def __init__(self, px, py, bonusNum):
+    """Class representing a bonus in the game.
+
+    Attributes:
+        px (int): Horizontal position of the bonus.
+        py (int): Vertical position of the bonus.
+        bonusnum (int): Type of the bonus.
+
+    Methods:
+        __init__(self, px, py, bonusnum): Initializes a new bonus instance.
+        update(self): Handles the bonus timer and interactions with tanks.
+        draw(self): Draws the bonus on the game window.
+    """
+
+    def __init__(self, px, py, bonusnum):
+        """Initializes a new bonus with given parameters.
+
+        Args:
+            px (int): Horizontal position of the bonus.
+            py (int): Vertical position of the bonus.
+            bonusnum (int): Type of the bonus.
+        """
         # Добавляет бонус в объекты
         objects.append(self)
         # Присваивание типа бонуса
         self.type = 'bonus'
         # Картинка из списка
-        self.image = imgBonuses[bonusNum]
+        self.image = imgBonuses[bonusnum]
         # Создание координат
         self.rect = self.image.get_rect(center=(px, py))
-        # Таймер
+        # Таймер (В ФПС)
         self.timer = 600
         # Сохранение номера бонуса
-        self.bonusNum = bonusNum
+        self.bonusNum = bonusnum
 
-    # Обновление
     def update(self):
+        """Updates the bonus state, including its timer and interaction with tanks."""
         # Пока таймер больше 0 уменьшает его
         if self.timer > 0:
             self.timer -= 1
@@ -356,30 +459,50 @@ class Bonus:
             # Если объект танк и его координаты сходятся с бонусом
             if obj.type == 'tank' and self.rect.colliderect(obj.rect):
                 # Если бонус №0, то увеличивает ранг пока это возможно
-                if self.bonusNum == 0:
-                    if obj.rank < len(imgTanks) - 1:
-                        obj.rank += 1
-                        objects.remove(self)
-                        # Звук
-                        sound_star.play()
-                        break
+                if self.bonusNum == 0 and obj.rank < len(imgTanks) - 1:
+                    obj.rank += 1
+                    objects.remove(self)
+                    # Звук
+                    sound_star.play()
+                    break
+                # Если бонус №1, то добавляет здоровье
                 elif self.bonusNum == 1:
-                    # Если бонус №1, то добавляет здоровье
+
                     obj.hp += 1
                     objects.remove(self)
+                    # Звук
                     sound_star.play()
                     break
 
-    # Отрисовка объекта с условием в зависимости от времени таймера
+    # Отрисовка объекта с условием в зависимости от времени таймера (будет мигать)
     def draw(self):
-        # Условие для мигания бонуса
+        """Draws the bonus on the game window."""
         if self.timer % 30 < 15:
             window.blit(self.image, self.rect)
 
-# Класс логотипа ВШЭ
+
 class HSE:
-    # Указание координат и размера (32х32)
+    """Class representing the HSE logo in the game.
+
+    Attributes:
+        px (int): Horizontal position of the logo.
+        py (int): Vertical position of the logo.
+        size (int): Size of the logo.
+
+    Methods:
+        __init__(self, px, py, size): Initializes the HSE logo instance.
+        update(self): Currently does nothing (placeholder for future functionality).
+        draw(self): Draws the HSE logo on the game window.
+    """
+
     def __init__(self, px, py, size):
+        """Initializes the HSE logo with given parameters.
+
+        Args:
+            px (int): Horizontal position of the logo.
+            py (int): Vertical position of the logo.
+            size (int): Size of the logo.
+        """
         # Добавление в объекты
         objects.append(self)
         # Добавление типа
@@ -389,23 +512,26 @@ class HSE:
 
     # Объект не обновляется и статичен, но отсутствие update() вызовет ошибку
     def update(self):
+        """Updates the state of the HSE logo. (Currently does nothing)"""
         pass
 
     # Отрисовка логотипа
     def draw(self):
+        """Draws the HSE logo on the game window."""
         window.blit(imgHSE, self.rect)
+
 
 # Здесь хранится информация о пулях
 bullets = []
 # Здесь хранятся все объекты, которые есть в игре
 objects = []
 # Создание танка_1 (WASD SPACE 6)
-Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE, pygame.K_6))
+Tank('blue', 100, 275, 0, [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE, pygame.K_6])
 # Создание танка_2 (Стрелочки BACKSPACE 9)
-Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_BACKSPACE, pygame.K_9))
+Tank('red', 650, 275, 0, [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_BACKSPACE, pygame.K_9])
 # Проигрывание основной мелодии во время игры
 sound_theme.play()
-# Этот объект будет обновлять данные
+# Этот объект будет обновлять данные в пользовательском интерфейсе
 ui = UI()
 
 # Цикл для создания блоков в случайном месте, кроме координат с другими объектами 50 раз
@@ -429,11 +555,10 @@ for _ in range(50):
     # Вызвать функцию блока, если все прошло
     Block(x, y, TILE)
 # Создание логотипа на поле боя (в нем можно спрятаться и нельзя сломать)
-HSE(770,1, 1)
+HSE(770, 1, 1)
 # Стартовое значения интервала времени для подбора бонусов
 bonusTimer = 180
-
-# Шаблон запуска игры
+# Шаблон запуска игры на pygame
 play = True
 while play:
     for event in pygame.event.get():
@@ -450,12 +575,13 @@ while play:
         # Генерация случайного интервала времени
         bonusTimer = randint(120, 240)
     # Обновление статусов пуль в игре
-    for bullet in bullets: bullet.update()
+    for bullet in bullets:
+        bullet.update()
     # Обновление статусов объектов
-    for obj in objects: obj.update()
+    for obj in objects:
+        obj.update()
     # Обновление счетчика жизней и отображение рангов
     ui.update()
-
     # Окраска фона игры в черный
     window.fill('black')
     # Отрисовка пуль
@@ -466,6 +592,7 @@ while play:
         obj.draw()
     # Отрисовка интерфейса
     ui.draw()
+
     # Шаблон pygame
     pygame.display.update()
     clock.tick(FPS)
